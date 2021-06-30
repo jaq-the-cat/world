badf3b2541136a974da8e4cbffa9b994f7fa5d40
jaq-cat <jaq.cat@protonmail.ch>
2021-06-30 15:53:12 -0300

    camera working

--- src/main.c
+++ src/main.c
@@ -2,11 +2,36 @@
 #include <SDL2/SDL_image.h>
 
 #define WIDTH 800
-#define HEIGHT 800
+#define HEIGHT 600
+#define WWIDTH 2000
+#define WHEIGHT 2000
+
 #define BG 200
+
+#define SPD 20
+
+static int camX = 0, camY = 0;
 
 SDL_Window *win;
 SDL_Renderer *rend;
+
+SDL_Texture *boi;
+
+struct {
+    char w, a, s, d;
+} keyboard;
+
+// world (get from res/world.json later)
+enum Locs {
+    X, Y
+};
+
+int world[][2] = {
+    {0, 0},
+    {50, 50},
+    {100, 100},
+    {0, 200},
+};
 
 // initialization
 void init() {
@@ -21,20 +46,57 @@ void init() {
         0);
     rend = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
     IMG_Init(IMG_INIT_PNG);
+
+    boi = IMG_LoadTexture(rend, "res/boi.png");
 }
 
 // rendering
 void render() {
+    SDL_Rect display;
+    for (int i=0; i<4; i++) {
+        display.x = world[i][X] - camX;
+        display.y = world[i][Y] - camY;
+        display.w = 40;
+        display.h = 40;
+        SDL_RenderCopy(rend, boi, NULL, &display);
+    }
 }
 
 // processing
 int handleev() {
     SDL_Event event;
     SDL_PollEvent(&event);
-    return event.key.keysym.scancode == SDL_SCANCODE_Q;
+    switch (event.key.keysym.scancode) {
+        case SDL_SCANCODE_Q:
+            return 1;
+        case SDL_SCANCODE_W:
+            keyboard.w = event.key.type == SDL_KEYDOWN;
+            break;
+        case SDL_SCANCODE_A:
+            keyboard.a = event.key.type == SDL_KEYDOWN;
+            break;
+        case SDL_SCANCODE_S:
+            keyboard.s = event.key.type == SDL_KEYDOWN;
+            break;
+        case SDL_SCANCODE_D:
+            keyboard.d = event.key.type == SDL_KEYDOWN;
+            break;
+        default:
+            break;
+    }
+    return 0;
 }
 
 void update() {
+    if (keyboard.w)
+        camY += SPD;
+    if (keyboard.s)
+        camY -= SPD;
+
+    if (keyboard.a)
+        camX += SPD;
+    if (keyboard.d)
+        camX -= SPD;
 }
 
 int main() {
